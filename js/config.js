2a4661ab66ac8452191e285339d3f30f533efcce
Linda van den Brink <l.vandenbrink@geonovum.nl>
2023-04-20 09:13:33 +0200

    Revert "update config"
    This reverts commit 70fcd47188ea9eb78f4c7619cc98bf4e2d7d8cd7.

--- js/config.js
+++ js/config.js
@@ -1,61 +1,20 @@
-//--              Deze file moet worden neergezet in de root-directory van de 
-//--              betreffende standaard. 
-//--              Deze file bevat de configuratie opties voor het respec document.
-
 let respecConfig = {
   useLogo: true,
   useLabel: true,
-  //-- license: voor de geldende gebruiksvoorwaarden. Default is cc-by.
-  //licence: "cc-by-nd",            // bronvermelding, geen afgeleide werken (default)
-  //licence: "cc0",                 // Public Domain Dedication
-  licence: "cc-by",                 // Attribution, met bronvermelding
-  //-- specStatus is verplicht! (activeer 1 van de volgende) --------------------------
-  //specStatus: "GN-BASIS",             // Basis Document
-  specStatus: "GN-WV",              // Werk Versie
-  //specStatus: "GN-CV",              // Consultatie Versie
-  //specStatus: "GN-VV",              // Vaststellings Versie
-  //specStatus: "GN-DEF",             // Definitieve Versie
-  //-- specType is verplicht bij alle andere dan BASIS ---------------------------------
-  //specType: "NO",                   // Norm
-  //specType: "ST",                   // Standaard
-  //specType: "IM",                   // Informatie Model
-  //specType: "PR",                   // Praktijkrichtlijn
-  specType: "HR",                   // HandReiking
-  //specType: "WA",                   // Werkafspraak
-  //specType: "BD",                   // Beheer Documentatie
-  //specType: "AL",                   // Algemeen document
-  //specType: "BP",                   // Best Practice
-  //-- pubDomain is verplicht! (komt in de URL: Activeer 1 van de volgende) ------------
-  pubDomain: "dk", 
-  //pubDomain: "et", 	              // Energie transitie
-  //pubDomain: "mim", 	            // Metamodel Informatie Modellering
-  //pubDomain: "bor", 	            // Beheer Openbare Ruimte
-  //pubDomain: "bro", 	            // Basisregistratie Ondergrond
-  //pubDomain: "imgeo", 	          // IMGeo / BGT
-  //pubDomain: "kl", 	              // Kabels en Leidingen
-  //pubDomain: "liv", 	            // Landelijke Informatievoorziening Vastgoedgebruik
-  //pubDomain: "md", 	              // Metadata
-  //pubDomain: "nen3610", 	        // Basismodel NEN3610
-  //pubDomain: "oov", 	            // Openbare Orde en Veiligheid
-  //pubDomain: "ro", 	              // Ruimtelijke Ordening
-  //pubDomain: "serv", 	            // Services
-  //pubDomain: "visu", 	            // Visualisatie
-  //pubDomain: "wp", 	              // White Paper
-  //-- shortName is verplicht! (komt in de URL: kies logische naam) --------------------
+  //default voor Geonovum is cc-by, eventueel is het mogelijk een andere licentie (cc0 of cc-by-nd) hier te kiezen voor het document.
+  //license: "cc-by",
+  specStatus: "CV",
+  specType: "HR",
+  pubDomain: "dk",
   shortName: "NL-ReSpec-GN-template",
-  //-- publishDate is verplicht -------------------------------------------------------
-  //-- NB: in de werkversie uitzetten, want dan pakt Respec de pushdate ---------------
-  //publishDate: "2023-03-28",
+  publishDate: "2023-03-28",
   //eventueel is het mogelijk een versienummer mee te geven, maar bij Geonovum werken we gewoonlijk alleen met datum als onderdeel van de permanente URI.
   //publishVersion: "0.0.2",
-  //-- Voor dit blok geldt: alleen als er eerdere versies zijn en altijd beiden aan/uit! 
   //previousVersion: "0.0.1",
-  //previousPublishDate: "2014-05-01",
-  //previousMaturity: "CV",
+  previousPublishDate: "2014-05-01",
+  previousMaturity: "CV",
   title: "[Neem titel op in config.js]",
-  //-- de namen van de Editor(s) / Redacteur(en)---------------------------------------
-  //-- vul in: per Editor: name:, company:, companyURL: -------------------------------
-    editors:
+  editors:
     [
       {
         name: "voornaam achternaam",
@@ -63,8 +22,6 @@ let respecConfig = {
         companyURL: "https://www.geonovum.nl",
       }
     ],
-  //-- de namen van de auteur(s) ------------------------------------------------------
-  //-- vul in: per auteur: name:, company:, companyURL: -------------------------------
   authors:
     [
       {
@@ -75,44 +32,8 @@ let respecConfig = {
     ],
   //neem hier de URL van de github repository op waar het respec document in staat
   github: "https://github.com/geonovum/NL-ReSpec-GN-template",
-  //-- localBiblio: lokale bibliografie, voor verwijzigingen
-  //-- NB: kijk eesrt naar de beschikbare www.specref.org voor verwijziging 
-  localBiblio: 
-  {
-    "PUB-1":
-      {
-        title:      "Titel van Publicatie-1",
-        href:       "https://www.geonovum.nl",
-        status:     "V1.0.1",
-        publisher:  "Publisher-1",
-        company:    "Companynaam",
-    },
-    "PUB-2":
-      {
-        title:      "Titel van Publicatie-2",
-        href:       "https://www.geonovum.nl",
-        status:     "V1.0.1",
-        publisher:  "Publisher-2",
-        company:    "Companynaam",
-    },
-    "PUB-3":
-      {
-        title:      "Titel van Publicatie-3",
-        href:       "https://www.geonovum.nl",
-        status:     "V1.0.1",
-        publisher:  "Publisher-3",
-        company:    "Companynaam",
-    },
-    "PUB-4":
-      {
-        title:      "Titel van Publicatie-4",
-        href:       "https://www.geonovum.nl",
-        status:     "V1.0.1",
-        publisher:  "Publisher-4",
-        company:    "Companynaam",
-    },
-  },
-  //maxTocLevel: 3,                           // Aantal niveau's ToC, default is 0
+
+
   // Create PDF and link to file in header (optional):
   alternateFormats: [
       {
