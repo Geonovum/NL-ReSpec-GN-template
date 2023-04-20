da7caa853fd07f682df2b94d5a75204ceb3774c1
Linda van den Brink <l.vandenbrink@geonovum.nl>
2023-04-20 14:03:05 +0200

    update config
    gedocumenteerde versie

--- js/config.js
+++ js/config.js
@@ -1,19 +1,44 @@
 let respecConfig = {
   useLogo: true,
   useLabel: true,
-  //default voor Geonovum is cc-by, eventueel is het mogelijk een andere licentie (cc0 of cc-by-nd) hier te kiezen voor het document.
-  //license: "cc-by",
-  specStatus: "wv",
+  // title is verplicht! Neem hier de titel van het document op ----------------------
+  title: "[Neem titel op in config.js]",
+  //-- specStatus is verplicht! (activeer 1 van de volgende) --------------------------
+  specStatus: "wv",                   // Werkversie
+  //specStatus: "cv",               // Consultatieversie
+  //specStatus: "vv",               // Versie ter vaststelling
+  //specStatus: "def",              // Vastgestelde versie
+  //specStatus: "basis",            // Basis Document
+  //-- specType is verplicht bij alle andere dan BASIS ---------------------------------
+  //specType: "NO",                 // Norm
+  //specType: "ST",                 // Standaard
+  //specType: "IM",                 // Informatie Model
+  //specType: "PR",                 // Praktijkrichtlijn
+  specType: "HR",                     // HandReiking
+  //specType: "WA",                 // Werkafspraak
+  //specType: "BD",                 // Beheer Documentatie
+  //specType: "AL",                 // Algemeen document
+  //specType: "BP",                 // Best Practice
   specType: "HR",
+  //-- pubDomain is verplicht! (komt in de URL) -------------------------------------
   pubDomain: "dk",
+  //-- license: voor de geldende gebruiksvoorwaarden. Default is cc-by.
+  //licence: "cc-by-nd",            // bronvermelding, geen afgeleide werken (default)
+  //licence: "cc0",                 // Public Domain Dedication
+  licence: "cc-by",                 // Attribution, met bronvermelding
+  //-- shortName is verplicht! (komt in de URL: kies logische afkorting)--------------
   shortName: "NL-ReSpec-GN-template",
-  publishDate: "2023-03-28",
+  //-- publishDate is verplicht -------------------------------------------------------
+  //-- NB: in de werkversie uitzetten, want dan pakt Respec de pushdate ---------------
+  //publishDate: "2023-03-28",
   //eventueel is het mogelijk een versienummer mee te geven, maar bij Geonovum werken we gewoonlijk alleen met datum als onderdeel van de permanente URI.
   //publishVersion: "0.0.2",
   //previousVersion: "0.0.1",
-  previousPublishDate: "2014-05-01",
-  previousMaturity: "CV",
-  title: "[Neem titel op in config.js]",
+  //-- Voor dit blok geldt: alleen als er eerdere versies zijn en altijd beiden aan/uit! 
+  //previousPublishDate: "2014-05-01",
+  //previousMaturity: "CV",
+  //-- de namen van de Editor(s) / Redacteur(en)---------------------------------------
+  //-- vul in: per Editor: name:, company:, companyURL: -------------------------------
   editors:
     [
       {
@@ -22,6 +47,8 @@ let respecConfig = {
         companyURL: "https://www.geonovum.nl",
       }
     ],
+  //-- de namen van de auteur(s) ------------------------------------------------------
+  //-- vul in: per auteur: name:, company:, companyURL: -------------------------------
   authors:
     [
       {
@@ -32,8 +59,6 @@ let respecConfig = {
     ],
   //neem hier de URL van de github repository op waar het respec document in staat
   github: "https://github.com/geonovum/NL-ReSpec-GN-template",
-
-
   // Create PDF and link to file in header (optional):
   alternateFormats: [
       {
